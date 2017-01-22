f108ca15d3577c2ad47bc3f1647d0184bb752e58
Brad Searle <Brad Searle>
2017-01-22 16:37:57 -0600

    Norris jokes first submission

--- index.js
+++ index.js
@@ -2,30 +2,38 @@ var Alexa = require('alexa-sdk');
 var http = require('http');
 
 var APP_ID = undefined; // TODO Get app id
+var alexa;
 
 var messages = {
+  appName: "Norris Jokes",
   introText: "Here's your joke. ",
-  helpMessage: "You can ask for a Chuck Norris joke, or request a joke about a specific person. You can also ask for a nerdy joke or an explicit joke."
+  helpMessage: "Welcome to Norris Jokes. You can ask for a Chuck Norris joke, or request a joke about a specific person. You can also ask for a nerdy joke or an explicit joke.",
+  errorMessage: "Sorry, there was an error while retrieving your joke. Please try again later.",
+  unhandledErrorMessage: "We couldn't understand your request. Please try again."
 }
+
 
 var handlers = {
   'LaunchRequest': function() {
-    this.emit('randomJokeIntent');
+    this.emit('AMAZON.HelpIntent');
   },
   'randomJokeIntent': function() {
-
+    httpGet(null);
   },
   'personJokeIntent': function() {
-
+    httpGet('firstName=' + this.event.request.intent.slots.person.value);
   },
   'categoryJokeIntent': function() {
-
+    httpGet('limitTo=' + this.event.request.intent.slots.jokeCategory.value);
   },
   'personCategoryJokeIntent': function() {
-
+    httpGet('limitTo=' + this.event.request.intent.slots.jokeCategory.value + '&firstName=' + this.event.request.intent.slots.person.value);
   },
-  'AMAZON.HELPINTENT': function() {
+  'AMAZON.HelpIntent': function() {
     this.emit(':tell', messages.helpMessage);
+  },
+  'Unhandled': function() {
+    this.emit(':tell', messages.unhandledErrorMessage);
   }
 }
 
@@ -34,4 +42,36 @@ exports.handler = function(event, context, callback) {
   alexa.APP_ID = APP_ID;
   alexa.registerHandlers(handlers);
   alexa.execute();
+}
+
+function httpGet(query, callback) {
+  var options = {
+    host: 'api.icndb.com',
+    path: '/jokes/random?escape=javascript' + (query ? '&' + query : ''),
+    method: 'GET'
+  };
+
+  var req = http.request(options, (res) => {
+
+        var body = '';
+
+        res.on('data', (d) => {
+            body += d;
+        });
+
+        res.on('end', function () {
+          var res = JSON.parse(body);
+          if( res.type === 'success' ) {
+            alexa.emit(':tellWithCard', messages.introText + res.value.joke, messages.appName, res.value.joke);
+          } else {
+            alexa.emit(':tell', messages.errorMessage);
+          }
+        });
+
+    });
+    req.end();
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
 }
